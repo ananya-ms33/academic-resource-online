78dd7a4c737066e050994ecbc97abf78a0353145
Ananya <ananyams003@gmail.com>
2026-03-12 22:20:34 +0530

    Initial commit / latest changes

--- frontend/admin-angular/src/app/admin/admin.ts
+++ frontend/admin-angular/src/app/admin/admin.ts
@@ -3,57 +3,42 @@ import { HttpClient } from '@angular/common/http'
 import { CommonModule } from '@angular/common'
 
 @Component({
-selector:'app-admin',
-standalone:true,
-imports:[CommonModule],
-templateUrl:'./admin.html'
+  selector: 'app-admin',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './admin.html'
 })
+export class AdminComponent {
 
-export class AdminComponent{
+  resources: any[] = []
+  experiences: any[] = []
 
-resources:any[]=[]
-experiences:any[]=[]
+  constructor(private http: HttpClient) {
+    this.loadData()
+  }
 
-constructor(private http:HttpClient){}
+  // Load data from server
+  loadData() {
+    this.http.get('http://localhost:3000/resources').subscribe((data: any) => {
+      this.resources = data
+    })
 
-ngOnInit(){
-this.load()
-}
+    this.http.get('http://localhost:3000/experiences').subscribe((data: any) => {
+      this.experiences = data
+    })
+  }
 
-load(){
+  // Delete resource
+  deleteresource(id: string) {
+    this.http.delete(`http://localhost:3000/deleteresource/${id}`).subscribe(() => {
+      this.resources = this.resources.filter(r => r._id !== id)
+    })
+  }
 
-this.http.get<any[]>("http://localhost:3000/resources")
-.subscribe(data=>{
-this.resources=data
-})
-
-this.http.get<any[]>("http://localhost:3000/experiences")
-.subscribe(data=>{
-this.experiences=data
-})
-
-}
-
-deleteresource(id:string){
-
-this.http.delete("http://localhost:3000/deleteresource/"+id)
-.subscribe(()=>{
-
-this.resources=this.resources.filter(r=>r._id!==id)
-
-})
-
-}
-
-deleteexperience(id:string){
-
-this.http.delete("http://localhost:3000/deleteexperience/"+id)
-.subscribe(()=>{
-
-this.experiences=this.experiences.filter(e=>e._id!==id)
-
-})
-
-}
-
+  // Delete experience
+  deleteexperience(id: string) {
+    this.http.delete(`http://localhost:3000/deleteexperience/${id}`).subscribe(() => {
+      this.experiences = this.experiences.filter(e => e._id !== id)
+    })
+  }
 }
